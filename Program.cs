703ab60a4ff744190fef282be21265ccef91e93c
26JVG01-JavierVasquez <vasquezjavier16db@gmail.com>
2026-03-19 12:11:30 -0600

    Add files via upload
    Ejercicio en clases

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    //verificar si un valor es positivo o negtivo
+    private static void Main(string[] args)
+    {
+        //entradas
+        Console.WriteLine("-----NUMEROS POSITIVOS Y NEGATIVOS------");
+        //proceso
+        int num;
+        Console.WriteLine("Ingrese un numero: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num >= 0) 
+        {
+            Console.WriteLine("El numero es positivo");
+        }
+        else
+        {
+            Console.WriteLine("El numero es negativo");
+        }
+    }
+}
