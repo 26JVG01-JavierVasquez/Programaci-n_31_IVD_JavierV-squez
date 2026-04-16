f723fba195233cdac31ccfd5c1bb4f7b824d28ea
26JVG01-JavierVasquez <vasquezjavier16db@gmail.com>
2026-04-16 11:42:28 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,49 @@
 ﻿internal class Program
 {
-    //verificar si un valor es positivo o negtivo
     private static void Main(string[] args)
     {
-        //entradas
-        Console.WriteLine("-----NUMEROS POSITIVOS Y NEGATIVOS------");
-        //proceso
-        int num;
-        Console.WriteLine("Ingrese un numero: ");
-        num = Convert.ToInt32(Console.ReadLine());
-        if (num >= 0) 
+        Console.WriteLine("Escribir dos variables numericos");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("-------MENÜ PRINSIPAL--------");
+        Console.WriteLine("1- Suma");
+        Console.WriteLine("2- Resta");
+        Console.WriteLine("3- Multiplicacion");
+        Console.WriteLine("4- Division");
+        Console.WriteLine("5- Salir");
+        Console.WriteLine("Digite la opcion que desea realizar: ()");
+        int opcion = Convert.ToInt32(Console.ReadLine());
+        string msg = "";
+        double Result = 0;
+        switch (opcion)
         {
-            Console.WriteLine("El numero es positivo");
+            case 1:
+                msg = "\n suma \n es:";
+                Result =num1 + num2;
+                break;
+            case 2:
+                msg = "\n resta \n es:";
+                Result = num1 - num2;
+                break;
+            case 3:
+                msg = "\n multiplicacion \n es:";
+                Result = num1 * num2;
+                break;
+            case 4:
+                msg = "\n division \n es:";
+                Result = num1 / num2;
+                break;
+            case 5:
+                msg = "Saliendo del programa...";
+                break;
+            default:
+                msg = "Digite un numero correcto";
+                break;
+
         }
-        else
-        {
-            Console.WriteLine("El numero es negativo");
-        }
+        Console.clear();
+        Console.WriteLine(msg);
+        Console.WriteLine(Result);
+
     }
 }
