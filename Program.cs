3b26a8010637bf5025293ffb22adf7fd1984c4e6
26JVG01-JavierVasquez <vasquezjavier16db@gmail.com>
2026-04-16 12:03:29 -0600

    Programa 2 C#

--- Program.cs
+++ Program.cs
@@ -2,48 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Escribir dos variables numericos");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-        double num2 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("-------MENÜ PRINSIPAL--------");
-        Console.WriteLine("1- Suma");
-        Console.WriteLine("2- Resta");
-        Console.WriteLine("3- Multiplicacion");
-        Console.WriteLine("4- Division");
-        Console.WriteLine("5- Salir");
-        Console.WriteLine("Digite la opcion que desea realizar: ()");
-        int opcion = Convert.ToInt32(Console.ReadLine());
-        string msg = "";
-        double Result = 0;
-        switch (opcion)
+        Console.WriteLine("Rango de estudiante");
+        Console.WriteLine("Ingrese una letra A,B,C,D,F");
+        char letra = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        switch (letra)
         {
-            case 1:
-                msg = "\n suma \n es:";
-                Result =num1 + num2;
+            case 'A':
+                Console.WriteLine("Excelente");
                 break;
-            case 2:
-                msg = "\n resta \n es:";
-                Result = num1 - num2;
+            case 'B':
+                Console.WriteLine("bueno");
                 break;
-            case 3:
-                msg = "\n multiplicacion \n es:";
-                Result = num1 * num2;
+            case 'C':
+                Console.WriteLine("regular");
                 break;
-            case 4:
-                msg = "\n division \n es:";
-                Result = num1 / num2;
+            case 'D':
+                Console.WriteLine("Deficiente");
                 break;
-            case 5:
-                msg = "Saliendo del programa...";
+            case 'F':
+                Console.WriteLine("Reprobado");
                 break;
             default:
-                msg = "Digite un numero correcto";
+                Console.WriteLine("Letra no válida");
                 break;
-
         }
-        Console.clear();
-        Console.WriteLine(msg);
-        Console.WriteLine(Result);
-
-    }
+        }
 }
